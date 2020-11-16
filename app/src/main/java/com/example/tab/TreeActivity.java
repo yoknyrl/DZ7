6c01487877cae711f629fe4fa89fd89c0484f161
Sibirien in meiner Seele <dima.pichen@gmail.com>
2020-11-16 15:20:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tab/TreeActivity.java
@@ -0,0 +1,21 @@
+package com.example.tab;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class TreeActivity extends Fragment {
+
+    @Nullable
+    @Override
+
+    public View onCreateView (@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @Nullable Bundle saveIn)
+    {
+        return  inflater.inflate(R.layout.tree_main, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/tab/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,61 @@
+package com.example.tab.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.tab.R;
+import com.example.tab.SevondActivity;
+import com.example.tab.TreeActivity;
+import com.example.tab.firstActivity;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position)
+        {
+            case 0:
+                firstActivity Tab1 = new firstActivity();
+                return Tab1;
+
+            case 1:
+                SevondActivity Tab2 = new SevondActivity();
+                return Tab2;
+
+            case 2:
+                TreeActivity Tab3 = new TreeActivity();
+                return Tab3;
+        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 3;
+    }
+}
